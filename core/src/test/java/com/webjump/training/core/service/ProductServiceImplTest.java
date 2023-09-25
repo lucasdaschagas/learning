b4f0b688bddb9587bdb81d6baa4e75f13bf601ce
Lucas das Chagas <lucaschagas379@gmail.com>
2023-09-25 17:23:49 -0300

    Service test updated, schedule test begins

--- core/src/test/java/com/webjump/training/core/service/ProductServiceImplTest.java
+++ core/src/test/java/com/webjump/training/core/service/ProductServiceImplTest.java
@@ -3,65 +3,48 @@ package com.webjump.training.core.service;
 import com.webjump.training.core.services.ProductServiceImpl;
 import io.wcm.testing.mock.aem.junit5.AemContext;
 import io.wcm.testing.mock.aem.junit5.AemContextExtension;
-import org.apache.sling.api.SlingHttpServletResponse;
-import org.apache.sling.api.resource.PersistenceException;
-import org.apache.sling.api.resource.Resource;
-import org.apache.sling.api.resource.ResourceResolver;
-import org.apache.sling.api.resource.ResourceResolverFactory;
+import org.apache.sling.api.resource.*;
 import org.apache.sling.testing.mock.sling.ResourceResolverType;
 import org.apache.sling.testing.mock.sling.servlet.MockSlingHttpServletRequest;
 import org.apache.sling.testing.mock.sling.servlet.MockSlingHttpServletResponse;
-import org.json.JSONException;
-import org.json.JSONObject;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.verify;
+import java.io.IOException;
 
 
 @ExtendWith({MockitoExtension.class, AemContextExtension.class})
 public class ProductServiceImplTest {
 
-    @Mock
-    private ResourceResolver resolver;
-    @Mock
-    private Resource resource;
-    @Mock
-    private ResourceResolverFactory factory;
     @InjectMocks
     private ProductServiceImpl product;
-    private AemContext context = new AemContext(ResourceResolverType.RESOURCERESOLVER_MOCK);
+
+    @Mock
+    private ResourceResolverFactory factory;
+
+    private final AemContext context = new AemContext(ResourceResolverType.RESOURCERESOLVER_MOCK);
 
     @BeforeEach
     public void setup() {
         context.registerService(ResourceResolverFactory.class, factory);
+
     }
 
     @Test
-    void testSaveProduct() throws PersistenceException, JSONException {
-            String requestJson = "{\"jcr:title\"ProductOfMine\"," +
-                                 "\"primaryType\":\"cq:Component\","+
-                                 "\"name\":\"otherName\"," +
-                                 "\"description\":\"anotherDescription\"}";
+    void testSaveProduct() throws IOException {
+        context.create().resource("/content/test", "jcr:title", "ServiceTest","name","","description","");
 
+        MockSlingHttpServletRequest request = context.request();
+        MockSlingHttpServletResponse response = context.response();
+        request.setMethod("POST");
+        request.addRequestParameter("name", "anotherName");
+        request.addRequestParameter("description", "anotherOneDescription");
+        request.setContentType("application/json");
 
-            MockSlingHttpServletRequest request = context.request();
-            MockSlingHttpServletResponse response = context.response();
-            request.setMethod("POST");
-            request.addRequestParameter("name","anotherName");
-            request.addRequestParameter("description","anotherDescriprion");
-            request.addHeader("Content-Type", "application/json");
-            request.setCharacterEncoding("UTF-8");
-            request.setContent(requestJson.getBytes());
+        product.saveProduct(request, response);
+        response.setContentType("application/json");
 
-
-
-            product.saveProduct(request,response);
-
-
-        }
     }
+}
